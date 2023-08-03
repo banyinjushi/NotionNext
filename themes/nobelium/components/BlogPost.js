41ff61a95122d701f6553abf8709f16f2754966d
banyinjushi <141348547+banyinjushi@users.noreply.github.com>
2023-08-04 02:09:38 +0800

    Update BlogPost.js

--- themes/nobelium/components/BlogPost.js
+++ themes/nobelium/components/BlogPost.js
@@ -6,11 +6,11 @@ const BlogPost = ({ post }) => {
   return (
     <Link href={`${BLOG.SUB_PATH}/${post.slug}`}>
       <article key={post.id} className="mb-6 md:mb-8">
-        <header className="grid grid-cols-2 items-baseline md:flex-row">
-          <time className="text-gray-600 dark:text-gray-400 mb-2 md:mb-0" style={{ width: '150px' }}>
+        <header className="grid grid-cols-3 items-baseline md:flex-row">
+          <time className="text-gray-600 dark:text-gray-400 col-span-1 mb-2 md:mb-0" style={{ width: '150px' }}>
             {formatDate(post?.publishDay || post.createdTime, BLOG.LANG)}
           </time>
-          <h2 className="text-lg md:text-xl font-medium cursor-pointer text-black dark:text-gray-100">
+          <h2 className="text-lg md:text-xl font-medium cursor-pointer text-black dark:text-gray-100 col-span-2">
             {post.title}
           </h2>
         </header>
@@ -23,5 +23,6 @@ const BlogPost = ({ post }) => {
     </Link>
   )
 }
+
 
 export default BlogPost
