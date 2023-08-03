c5e41fae95a1dab37cbdf08fcba4cdd43f07a2e9
banyinjushi <141348547+banyinjushi@users.noreply.github.com>
2023-08-04 01:54:20 +0800

    Update BlogPost.js
    show time before title

--- themes/nobelium/components/BlogPost.js
+++ themes/nobelium/components/BlogPost.js
@@ -4,16 +4,15 @@ import formatDate from '@/lib/formatDate'
 
 const BlogPost = ({ post }) => {
   return (
-    (<Link href={`${BLOG.SUB_PATH}/${post.slug}`}>
-
+    <Link href={`${BLOG.SUB_PATH}/${post.slug}`}>
       <article key={post.id} className="mb-6 md:mb-8">
         <header className="flex flex-col justify-between md:flex-row md:items-baseline">
-          <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
-            {post.title}
-          </h2>
-          <time className="flex-shrink-0 text-gray-600 dark:text-gray-400">
+          <time className="flex-shrink-0 text-gray-600 dark:text-gray-400 mb-2 md:mb-0">
             {formatDate(post?.publishDay || post.createdTime, BLOG.LANG)}
           </time>
+          <h2 className="text-lg md:text-xl font-medium cursor-pointer text-black dark:text-gray-100">
+            {post.title}
+          </h2>
         </header>
         <main>
           <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
@@ -21,8 +20,7 @@ const BlogPost = ({ post }) => {
           </p>
         </main>
       </article>
-
-    </Link>)
+    </Link>
   )
 }
 
