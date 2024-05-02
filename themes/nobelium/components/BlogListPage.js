1758f84a2fce60a996341d2c28da6e3f037b996d
banyinjushi <whdxlucas@gmail.com>
2024-05-02 18:29:55 +0800

    remove title

--- themes/nobelium/components/BlogListPage.js
+++ themes/nobelium/components/BlogListPage.js
@@ -18,7 +18,7 @@ export const BlogListPage = props => {
 
   return (
       <div className="w-full md:pr-12 my-6">
-            <h2 style={{ marginBottom: '3rem' }} className="text-2xl font-bold">Most Clicked:</h2>
+            {/* <h2 style={{ marginBottom: '3rem' }} className="text-2xl font-bold">推荐文章</h2> */}
             <div id="posts-wrapper" className="pl-4">
                 {/* {posts?.map(post => ( */}
                 {/* {posts?.filter(post => post.tags == '精选').map(post => ( */}
