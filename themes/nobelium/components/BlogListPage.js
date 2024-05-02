5a848ca8e512e8aca3b7f286d8e652ac7f069506
banyinjushi <whdxlucas@gmail.com>
2024-05-02 17:55:54 +0800

    add main page title

--- themes/nobelium/components/BlogListPage.js
+++ themes/nobelium/components/BlogListPage.js
@@ -18,8 +18,7 @@ export const BlogListPage = props => {
 
   return (
       <div className="w-full md:pr-12 my-6">
-            <h2 style={{ marginBottom: '3rem' }} className="text-3xl font-bold">Most Clicked:</h2>
-
+            <h2 style={{ marginBottom: '3rem' }} className="text-2xl font-bold">Most Clicked:</h2>
             <div id="posts-wrapper">
                 {/* {posts?.map(post => ( */}
                 {/* {posts?.filter(post => post.tags == '精选').map(post => ( */}
