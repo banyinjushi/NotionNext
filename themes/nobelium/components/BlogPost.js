0f08c9842953baa2cf97f6efbc5165edb8879a10
banyinjushi <whdxlucas@gmail.com>
2024-05-02 18:43:08 +0800

    remove title

--- themes/nobelium/components/BlogPost.js
+++ themes/nobelium/components/BlogPost.js
@@ -7,15 +7,15 @@ const BlogPost = ({ post }) => {
 
       <article key={post.id} className="mb-6 md:mb-8">
         <header className="flex flex-col justify-between md:flex-row md:items-baseline">
+          <span className="flex-shrink-0 text-gray-600 dark:text-gray-400">
+              {post.category}
+          </span>
           <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
             {post.title}
           </h2>
           <time className="flex-shrink-0 text-gray-600 dark:text-gray-400">
             {post?.publishDay}
           </time>
-          <span className="ml-4 text-sm">
-              ({post.category})
-          </span>
         </header>
         <main>
           <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
